809dc0c48075113addccca9a567ca7c44e1f23e0
cesarcamus32 <cesarcamus32@gmail.com>
2025-05-19 13:31:18 +0000

    Codigos modificados

--- sample-app.sh
+++ sample-app.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
+mkdir -p tempdir/templates tempdir/static
+cp sample_app.py tempdir/
+cp -r templates/* tempdir/templates/
+cp -r static/* tempdir/static/
 
-mkdir tempdir
-mkdir tempdir/templates
-mkdir tempdir/static
-
-cp sample_app.py tempdir/.
-cp -r templates/* tempdir/templates/.
-cp -r static/* tempdir/static/.
-
-echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "FROM python" > tempdir/Dockerfile
+echo "RUN pip install flask gunicorn" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 8080" >> tempdir/Dockerfile
-echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
+echo "WORKDIR /home/myapp" >> tempdir/Dockerfile
+echo "ENV PYTHONPATH /home/myapp" >> tempdir/Dockerfile
+echo "EXPOSE 8888" >> tempdir/Dockerfile
+echo "CMD gunicorn --bind 0.0.0.0:8888 sample_app:sample" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t sampleapp .
-docker run -t -d -p 8080:8080 --name samplerunning sampleapp
-docker ps -a 
+docker rm -f samplerunning 2>/dev/null
+docker run -d -p 8888:8888 --name samplerunning sampleapp
+docker ps -a
